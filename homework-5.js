e9be832912d8ee7f6780d389c16080ff2c8ad04d
VadimIlyasov <vzilyas89@gmail.com>
2025-11-12 23:11:40 +0500

    bug fixes

--- homework-5.js
+++ homework-5.js
@@ -1,51 +1,51 @@
 // Функция принимающая два параметра : город и температура воздуха.
 
-function infoOutsideTemperature(cityName, temperature) {
+function getOutsideTemperatureInfo(cityName, temperature) {
     console.log(`Сейчас в ${cityName} температура  — ${temperature} градусов по Цельсию`)
 }
 
-infoOutsideTemperature('Уфа', -7)
+getOutsideTemperatureInfo('Уфа', -7)
 
 // функция которая сравнивает со скоростью звука и выводит соответсвующий результат
 
 
 function checkSpeedOfSound(speed) {
     const SPEED_OF_SOUND = 343
-    if(speed > SPEED_OF_SOUND) {
+    if (speed > SPEED_OF_SOUND) {
         console.log('Сверхзвуковая скорость')
-    } else if(speed === SPEED_OF_SOUND) {
+    } else if (speed === SPEED_OF_SOUND) {
         console.log('Скорость звука')
     } else {
         console.log('Дозвуковая скорость')
     }
 }
 
-speedOfSound(327)
+checkSpeedOfSound(327)
 
 // функция проверяющая текущий бюджет на необходимое количество для покупки товара
 
-const productName = 'грейпфрукт'
-const price = 500
+const productName = 'грейпфрут'
+const priceProduct = 500
 
-function budgetCheck(amount) {
-    if(amount > price) {
+function checkBudget(amount) {
+    if(amount > priceProduct) {
         console.log(`${productName} приобретен.Спасибо за покупку!`)
     } else {
-        const residual = price - amount
+        const residual = priceProduct - amount
         console.log(`Вам не хватает ${residual}, пополните баланс`)
     }
 
 }
 
-budgetCheck(600)
+checkBudget(600)
 
 // функция преобразующая введенные данные в индекс массы тела и сравнивающая результат  
 
 function checkBodyMassIndex(weight, height) {
-    const index = weight/(height*height)
-    if(index <= 18.5) {
+    const index = weight / (height * height)
+    if (index <= 18.5) {
         console.log('Недостаточная масса тела')
-    } else if(index > 18.5 && index <= 25) {
+    } else if (index > 18.5 && index <= 25) {
         console.log('Масса и рост соответсвуют норме')
     } else {
         console.log('Избыточная масса тела')
