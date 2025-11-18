0a96addd09ce824e66b353c0f7b9fe615d850f36
VadimIlyasov <vzilyas89@gmail.com>
2025-11-18 11:08:15 +0500

    fixed bags

--- homework-6.js
+++ homework-6.js
@@ -13,7 +13,7 @@ const person = {
 
 console.log(person)
 
-// объект, который хранит данные об автомолбиле
+// объект, который хранит данные об автомобиле
 const car = {
     carMake: "hyundai",
     carModel: "accent",
@@ -22,16 +22,23 @@ const car = {
     boxType: "mechanical"
 }
 
-console.log(car)
+// добавление свойства вледелец авто в объект car одним значением
+
+const updateInfoCar = {...car, owner: {...person}}
+
+console.log(updateInfoCar)
 
 // функция, которая проверяет на наличие строки "максимальная скорость и если его нет то добавляет"
 
-function getMaxSpeed(anotherCar) {
-    return 'maxSpeed' in anotherCar ? anotherCar : anotherCar = {...anotherCar, maxSpeed: 180}
+function getMaxSpeedAndUpdateInfoCar(anotherCar) {
+    if ('maxSpeed' in anotherCar) {
+        return anotherCar
+    } else {
+        return anotherCar = {...anotherCar, maxSpeed: 180}
+    }
 }
 
-
-const updatedCar = getMaxSpeed(car);
+const updatedCar = getMaxSpeedAndUpdateInfoCar(car);
 console.log(updatedCar)
 
 //функцию, которая получает первым аргументом  — объект, а вторым аргументом — свойство объекта 
@@ -51,17 +58,60 @@ const vegetables = ['cucumber', 'onion', 'tomato', 'potato', 'garlic', 'carrot',
 // массив, состоящий из объектов
 
 const russianBooks = [
-    {bookName: 'Мертвые души', author: 'Н.В.Гоголь', yearOfEdition: 1842, coverColor: 'зеленая', genre: 'роман'},
-    {bookName: 'Война и Мир', author: 'Л.Н.Толстой', yearOfEdition: 1867, coverColor: 'серая', genre: 'роман-эпопея'},
-    {bookName: 'Тихий Дон', author: 'М.А.Шолохов', yearOfEdition: 1928, coverColor: 'синяя', genre: 'роман-эпопея'}
+    {
+        bookName: 'Мертвые души', 
+        author: 'Н.В.Гоголь', 
+        yearOfEdition: 1842, 
+        coverColor: 'зеленая', 
+        genre: 'роман'
+    },
+    {   bookName: 'Война и Мир', 
+        author: 'Л.Н.Толстой', 
+        yearOfEdition: 1867, 
+        coverColor: 'серая', 
+        genre: 'роман-эпопея'
+    },
+    {   
+        bookName: 'Тихий Дон', 
+        author: 'М.А.Шолохов', 
+        yearOfEdition: 1928, 
+        coverColor: 'синяя', 
+        genre: 'роман-эпопея'
+    }
 ]
 
 const englishBooks = [
-    {bookName: '1984', author: 'George Orwell', yearOfEdition: 1949, coverColor: 'red', genre: 'novel'},
-    {bookName: 'Robinson Crusoe', author: 'Daniel Defoe', yearOfEdition: 1731, coverColor: 'yellow', genre: 'novel'},
-    {bookName: 'Gulliver`s Travels', author: 'Jonathan Swift', yearOfEdition: 1726, coverColor: 'white', genre: 'novel'},
-    {bookName: 'The Lord of the Rings', author: 'John Ronald Reuel Tolkien', yearOfEdition: 2003, coverColor: 'blue', genre: 'novel'}
+    {
+        bookName: '1984', 
+        author: 'George Orwell', 
+        yearOfEdition: 1949, 
+        coverColor: 'red', 
+        genre: 'novel'
+    },
+    {
+        bookName: 'Robinson Crusoe', 
+        author: 'Daniel Defoe', 
+        yearOfEdition: 1731, 
+        coverColor: 'yellow', 
+        genre: 'novel'
+    },
+    {
+        bookName: 'Gulliver`s Travels', 
+        author: 'Jonathan Swift', 
+        yearOfEdition: 1726, 
+        coverColor: 'white', 
+        genre: 'novel'
+    },
+    {
+        bookName: 'The Lord of the Rings', 
+        author: 'John Ronald Reuel Tolkien', 
+        yearOfEdition: 2003, 
+        coverColor: 'blue', 
+        genre: 'novel'
+    }
 ]
+
+// объединение массивов
 
 const mergeBooks = [...russianBooks, ...englishBooks]
 console.log(mergeBooks)
