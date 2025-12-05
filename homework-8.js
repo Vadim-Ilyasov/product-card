1f907f1eb78a38c13ebaaa8efceaaede21cfb5c8
VadimIlyasov <vzilyas89@gmail.com>
2025-12-05 23:17:17 +0500

    fixed bugs

--- homework-8.js
+++ homework-8.js
@@ -1,64 +1,41 @@
 import { productCards } from "./product-cards.js";
-
 const productTemplate = document.getElementById('product-template');
 const productList = document.getElementById('product-list');
 
-
-
-function showCards(value) {
-  if(value <= productCards.length) {
-    for(let i = 0; i < value; i++) {
-    const product = productCards[i];
-    const productClone = productTemplate.content.cloneNode(true);
-    productClone.querySelector('.product-img').src = `/images/${product.img}.png`
-    productClone.querySelector('.product-category').textContent = product.category
-    productClone.querySelector('.product-name').textContent = product.name
-    productClone.querySelector('.product-description').textContent = product.description
-    productClone.querySelector('.product-compound').innerHTML = product.compound.map(item => `<li>${item}</li>`).join("")
-    productClone.querySelector('.product-price-label').textContent = product.priceLabel
-    productClone.querySelector('.product-price').textContent = `${product.price} ₽`
-    productList.appendChild(productClone) 
-    }
-    return value; 
-  } else {
-  return;
-  }
+function viewCards(count) {
+  const showCards = productCards.slice(0, count)
+  showCards.forEach(product => {
+  const productClone = productTemplate.content.cloneNode(true);
+  productClone.querySelector('.product-img').src = `/images/${product.img}.png`
+  productClone.querySelector('.product-category').textContent = product.category
+  productClone.querySelector('.product-name').textContent = product.name
+  productClone.querySelector('.product-description').textContent = product.description
+  productClone.querySelector('.product-compound').innerHTML = product.compound.map(item => `<li>${item}</li>`).join("")
+  productClone.querySelector('.product-price-label').textContent = product.priceLabel
+  productClone.querySelector('.product-price').textContent = `${product.price} ₽`
+  productList.appendChild(productClone) 
+  })
 }
-
-
 
 const cardsName = productCards.reduce((acc, product) => (acc + (acc ? "; " : "") + product.name))
 console.log(cardsName)
 
-
 const namesLikeKeys = productCards.reduce((acc, product) => {
-    const key = product.name
-    if (!acc[key]) {
-    acc[key] = []; 
-  }
-    acc[key].push(product.description)
-    return acc
+  acc.push({[product.name]: product.description})
+  return acc
 }, [])
 
 console.log(namesLikeKeys)
 
-
-
-  
 const requestAmountCards = productCards.reduce((acc, answer) => {
   answer = prompt('Сколько карточек отобразить? От 1 до 5');
-  const regex = /[0-9]/
-  if( regex.test(answer)) {
-    if(answer >= 1 && answer <= 5) {
-      acc.push(showCards(answer))
+  const amount = Number(answer)
+    if(amount >= 1 && amount <= 5) {
+      acc.push(viewCards(amount))
       return acc
     } else {
       return alert('количество карточек меньше требуемого запроса')
-    }
-  } else {
-   alert('Не было введено число или строка пустая')
-  }   
+    } 
 })
-
 
 console.log(requestAmountCards)
